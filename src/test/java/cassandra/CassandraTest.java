6c9364f615d806c78813ced247489f1ddc979f2f
Harmin Parra Rueda <harmin.rueda@gmail.com>
2025-09-20 16:29:41 +0200

    update

--- src/test/java/cassandra/CassandraTest.java
+++ src/test/java/cassandra/CassandraTest.java
@@ -18,6 +18,7 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.time.Duration;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
